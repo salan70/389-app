ef8b4392f72f38031193714af2a882089690bee0
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-09-05 17:25:41 +0900

    各関数の返り値を変更

--- assets/scraping/scraping.py
+++ assets/scraping/scraping.py
@@ -27,7 +27,7 @@ def crawl_team(url):
     except AttributeError as e:
         return e
 
-    return 'not Error'
+    return None
 
 def crawl_all_player(url):
     pos_list = ['投手', '捕手', '内野手', '外野手']
@@ -50,6 +50,6 @@ def crawl_player_per_pos(url, pos):
     except AttributeError as e:
         return e
 
-    return 'not Error'
+    return None
 
 teams = crawl_team(url='https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E3%83%97%E3%83%AD%E9%87%8E%E7%90%83%E9%81%B8%E6%89%8B%E4%B8%80%E8%A6%A7')
