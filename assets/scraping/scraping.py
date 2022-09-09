06eb204cd279b11eeb0179368d288950f51d5f9d
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-09-09 16:23:33 +0900

    打撃成績をDataFrameとして出力する関数を作成

--- assets/scraping/scraping.py
+++ assets/scraping/scraping.py
@@ -1,9 +1,11 @@
+from cgitb import html
 from os import link
 from re import I
 from turtle import title
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
 import time
 
@@ -17,7 +19,8 @@ class Scraping:
 
         try:
             bs = BeautifulSoup(html, 'html.parser')
-            league_list = ['セントラル・リーグ', 'パシフィック・リーグ']
+            # league_list = ['セントラル・リーグ', 'パシフィック・リーグ']
+            league_list = ['セントラル・リーグ']
             teams = []
             for league in league_list:
                 teams += bs.find('div', {'id' : 'bodyContent'}).find('span', {'id' : league}).parent.next_sibling.next_sibling.find_all('a')
@@ -26,7 +29,7 @@ class Scraping:
                 # print(team.get_text(), '-------------')
                 team_url = team.attrs['href']
                 self.__crawl_all_player(url=team_url)
-                time.sleep(5)
+                time.sleep(3)
 
         except AttributeError as e:
             return e
@@ -34,7 +37,8 @@ class Scraping:
         return None
 
     def __crawl_all_player(self, url):
-        pos_list = ['投手', '捕手', '内野手', '外野手']
+        # pos_list = ['投手', '捕手', '内野手', '外野手']
+        pos_list = ['捕手']
         for pos in pos_list:
             self.__crawl_player_per_pos(url=url, pos=pos)
 
@@ -49,13 +53,28 @@ class Scraping:
             bs = BeautifulSoup(html, 'html.parser')        
             players = bs.find('div', {'id' : 'bodyContent'}).find('span', {'id' : pos}).parent.next_sibling.next_sibling.find_all('a')
             for player in players:
+                player_url = player.attrs['href']
                 # print(player.get_text())
-                # TODO 各選手のページに遷移し、データを取得
-                None
+                self.__get_hitter_data(plane_url=player_url)
+                time.sleep(3)
+
         except AttributeError as e:
             return e
 
         return None
+
+    def __get_hitter_data(self, plane_url):
+        try:
+            formated_url = 'https://ja.wikipedia.org{}'.format(plane_url)
+            # 'O':OPSのO
+            search_word = 'O'
+            data_flame = pd.read_html(formated_url, match=search_word)
+
+            for data in data_flame:
+                print(data)
+        except:
+            return
+
 
 scraping = Scraping()
 teams = scraping.crawl_team(url='https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E3%83%97%E3%83%AD%E9%87%8E%E7%90%83%E9%81%B8%E6%89%8B%E4%B8%80%E8%A6%A7')
