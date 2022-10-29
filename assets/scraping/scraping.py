92012d7df72ba6bded67fe927261eef47e1eedcc
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-10-29 19:34:07 +0900

    野手の成績テーブルを取得できるよう更新

--- assets/scraping/scraping.py
+++ assets/scraping/scraping.py
@@ -26,7 +26,7 @@ class Scraping:
                 teams += bs.find('div', {'id' : 'bodyContent'}).find('span', {'id' : league}).parent.next_sibling.next_sibling.find_all('a')
 
             for team in teams:
-                print(team.get_text(), '-------------')
+                print(team.get_text(), '============================')
                 team_url = team.attrs['href']
                 self.__crawl_all_player(url=team_url)
                 time.sleep(2)
@@ -38,7 +38,8 @@ class Scraping:
 
     def __crawl_all_player(self, url):
         # pos_list = ['投手', '捕手', '内野手', '外野手']
-        pos_list = ['外野手']
+        pos_list = ['捕手', '内野手', '外野手']
+        # pos_list = ['外野手']
         for pos in pos_list:
             self.__crawl_player_per_pos(url=url, pos=pos)
 
@@ -53,12 +54,13 @@ class Scraping:
             bs = BeautifulSoup(html, 'html.parser')        
             players = bs.find('div', {'id' : 'bodyContent'}).find('span', {'id' : pos}).parent.next_sibling.next_sibling.find_all('a')
             for player in players:
-                # print(player.get_text())
+                print(player.get_text())
                 player_url = player.attrs['href']
                 self.__get_hitter_data(url=player_url)
 
                 # TODO playersへのinsert
                 # TODO 
+                print('----------------next----------------')
                 time.sleep(2)
 
         except AttributeError as e:
@@ -69,13 +71,12 @@ class Scraping:
     def __get_hitter_data(self, url):
         try:
             # '犠':犠打や犠飛の犠
-            # 野手の成績を取得するために設定
+            # 野手の成績を取得するために設定（wikipediaの表だと犠と打の間に改行が入るため、1文字で指定）
             search_word = '犠'
-            data_flame = pd.read_html('https://ja.wikipedia.org{}'.format(url), match=search_word)
 
-            # data_flame[0]のほうが良い？
-            for data in data_flame:
-                print(data)
+            data_flame = pd.read_html('https://ja.wikipedia.org{}'.format(url), match=search_word ,header=0)
+
+            print(data_flame[0])
 
             # TODO player_idをdfに追加
             # TODO idをdfに追加
