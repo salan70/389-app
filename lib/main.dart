cc3921819a1dfd90bdfac9a2af43b384901b3821
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-03 15:38:15 +0900

    📦 introduce : dotenv, supabaseを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // .envの読み込み
+  await dotenv.load(fileName: ".env");
+
+  // Supabaseの初期化
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_API_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
