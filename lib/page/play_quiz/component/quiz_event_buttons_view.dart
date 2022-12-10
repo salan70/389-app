1988e16bf4ad4d5e8a3109e6bef2ea4a8bbaa215
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-10 21:42:07 +0900

    ♻️ refactor : todoコメントを整理

--- lib/page/play_quiz/component/quiz_event_buttons_view.dart
+++ lib/page/play_quiz/component/quiz_event_buttons_view.dart
@@ -18,18 +18,18 @@ class QuizEventButtons extends ConsumerWidget {
             if (viewModel.canAddId()) {
               viewModel.addId();
             } else {
-              // TODO Buttonを非活性にする
+              // TODO Buttonを非活性にする（UIの見た目完成次第実装）
             }
           },
           child: const Text('次の成績を表示'),
         ),
         TextButton(
           onPressed: () {
-            // TODO 本当に良いか確認するダイアログ表示させる
+            // TODO 本当に良いか確認するダイアログ表示させる（UIの見た目完成次第実装）
             if (viewModel.canAddId()) {
               viewModel.addAllId();
             } else {
-              // TODO Buttonを非活性にする
+              // TODO Buttonを非活性にする（UIの見た目完成次第実装）
             }
           },
           child: const Text('全ての成績を表示'),
