b4e1e4bbb022693278bd53baa1c346afd48961a6
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-10 20:14:36 +0900

    ♻️ refactor : 不要なimportを削除

--- lib/page/play_quiz/component/quiz_view.dart
+++ lib/page/play_quiz/component/quiz_view.dart
@@ -1,7 +1,5 @@
-import 'package:baseball_quiz_app/model/ui/quiz_coordinate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:uuid/uuid.dart';
 
 import '../../../constant/stats_type.dart';
 import '../play_quiz_view_model.dart';
