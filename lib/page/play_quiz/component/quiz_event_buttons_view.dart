e68a8d84c38fd40401227c9d27ac74b72bed6ccb
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-10 21:40:12 +0900

    ♻️ refactor : quiz_event_buttonsを別ファイルに切り出し

---
+++ lib/page/play_quiz/component/quiz_event_buttons_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../play_quiz_view_model.dart';
+
+class QuizEventButtons extends ConsumerWidget {
+  const QuizEventButtons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(playQuizViewModelProvider);
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () {
+            if (viewModel.canAddId()) {
+              viewModel.addId();
+            } else {
+              // TODO Buttonを非活性にする
+            }
+          },
+          child: const Text('次の成績を表示'),
+        ),
+        TextButton(
+          onPressed: () {
+            // TODO 本当に良いか確認するダイアログ表示させる
+            if (viewModel.canAddId()) {
+              viewModel.addAllId();
+            } else {
+              // TODO Buttonを非活性にする
+            }
+          },
+          child: const Text('全ての成績を表示'),
+        ),
+      ],
+    );
+  }
+}
--- lib/page/play_quiz/play_quiz_view.dart
+++ lib/page/play_quiz/play_quiz_view.dart
@@ -1,7 +1,7 @@
-import 'package:baseball_quiz_app/page/play_quiz/play_quiz_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'component/quiz_event_buttons_view.dart';
 import 'component/quiz_view.dart';
 
 class PlayQuizView extends ConsumerWidget {
@@ -19,42 +19,6 @@ class PlayQuizView extends ConsumerWidget {
           QuizEventButtons(),
         ],
       ),
-    );
-  }
-}
-
-class QuizEventButtons extends ConsumerWidget {
-  const QuizEventButtons({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final viewModel = ref.watch(playQuizViewModelProvider);
-    return Row(
-      children: [
-        TextButton(
-          onPressed: () {
-            if (viewModel.canAddId()) {
-              viewModel.addId();
-            } else {
-              // TODO Buttonを非活性にする
-            }
-          },
-          child: const Text('次の成績を表示'),
-        ),
-        TextButton(
-          onPressed: () {
-            // TODO 本当に良いか確認するダイアログ表示させる
-            if (viewModel.canAddId()) {
-              viewModel.addAllId();
-            } else {
-              // TODO Buttonを非活性にする
-            }
-          },
-          child: const Text('全ての成績を表示'),
-        ),
-      ],
     );
   }
 }
