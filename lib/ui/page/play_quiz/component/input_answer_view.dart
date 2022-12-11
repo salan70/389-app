adc6c25ed6e5bbe4b72aac47e3f0e88c0453f9ca
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-11 20:41:11 +0900

    🚀 feat : クイズの結果を出力する処理を実装

--- lib/ui/page/play_quiz/component/input_answer_view.dart
+++ lib/ui/page/play_quiz/component/input_answer_view.dart
@@ -1,3 +1,4 @@
+import 'package:baseball_quiz_app/model/ui/hitter_map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:textfield_search/textfield_search.dart';
@@ -12,22 +13,35 @@ class InputAnswer extends ConsumerWidget {
     final scrollController = ScrollController();
     final textEditingController = TextEditingController();
     final viewModel = ref.watch(inputAnswerViewModelProvider);
+    final selectedHitterIdNotifier =
+        ref.watch(selectedHitterIdProvider.notifier);
 
-    return TextFieldSearch(
-        label: '選手名',
-        controller: textEditingController,
-        minStringLength: 0,
-        itemsInView: 5,
-        scrollbarDecoration: ScrollbarDecoration(
-          controller: scrollController,
-          theme: const ScrollbarThemeData(),
+    return Column(
+      children: [
+        TextFieldSearch(
+          label: '選手名',
+          controller: textEditingController,
+          minStringLength: 0,
+          itemsInView: 5,
+          scrollbarDecoration: ScrollbarDecoration(
+            controller: scrollController,
+            theme: const ScrollbarThemeData(),
+          ),
+          future: () async {
+            return await viewModel.searchHitter(textEditingController.text);
+          },
+          getSelectedValue: (HitterMap value) {
+            selectedHitterIdNotifier.state = value.id;
+          },
         ),
-        future: () async {
-          return await viewModel.searchHitter(textEditingController.text);
-        },
-        getSelectedValue: (value) {
-          // TODO 選択しているvalueをセット
-          print(value);
-        });
+        TextButton(
+          onPressed: () {
+            // TODO 結果のダイアログを表示
+            final result = viewModel.judgeQuizResult();
+          },
+          child: const Text('回答する'),
+        ),
+      ],
+    );
   }
 }
