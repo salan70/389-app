9728db62c830c6dc84032e540973231e5be233de
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-11 16:11:50 +0900

    🚀 feat : hitterMapクラスを実装

---
+++ lib/model/ui/hitter_map.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'hitter_map.freezed.dart';
+part 'hitter_map.g.dart';
+
+@freezed
+class HitterMap with _$HitterMap {
+  const factory HitterMap({
+    required String name,
+    required String id,
+  }) = _HitterMap;
+
+  factory HitterMap.fromJson(Map<String, dynamic> json) =>
+      _$HitterMapFromJson(json);
+}
