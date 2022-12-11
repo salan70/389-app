9ad0ca3df6b89f6dc9d52a22d17bf49bebcd01f4
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-11 16:11:50 +0900

    🚀 feat : hitterMapクラスを実装

---
+++ lib/model/ui/hitter_map.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hitter_map.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_HitterMap _$$_HitterMapFromJson(Map<String, dynamic> json) => _$_HitterMap(
+      name: json['name'] as String,
+      id: json['id'] as String,
+    );
+
+Map<String, dynamic> _$$_HitterMapToJson(_$_HitterMap instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'id': instance.id,
+    };
