0fc70f98bce7d80f2c04e8d02eefb98226d2cbfb
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-18 22:59:40 +0900

    ♻️ refactor : warningをignore

--- lib/model/ui/hitter_id_by_name.g.dart
+++ lib/model/ui/hitter_id_by_name.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: non_constant_identifier_names
+
 part of 'hitter_id_by_name.dart';
 
 // **************************************************************************
