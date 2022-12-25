2b7295355354d32ba7deb7a0eb7234044d371812
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-25 18:00:33 +0900

    ♻️ refactor : hitterSearchConditionProviderを移動

---
+++ lib/state/hitter_search_condition_state.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../constant/hitting_stats/stats_type.dart';
+import '../model/hitter_search_condition.dart';
+
+// TODO(me): 初期値をHiveで取得するようにする
+final hitterSearchConditionProvider = StateProvider<HitterSearchCondition>(
+  (_) => const HitterSearchCondition(
+    teamList: ['千葉ロッテマリーンズ'],
+    minGames: 0,
+    minHits: 0,
+    minPa: 0,
+    selectedStatsList: [
+      StatsType.year,
+      StatsType.team,
+      StatsType.avg,
+      StatsType.hr,
+      StatsType.ops,
+    ],
+  ),
+);
