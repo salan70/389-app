30ab0927406d292e09c826d9ab4c26bf4fa42159
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-25 17:15:42 +0900

    🚀 feat : チームのListをMap型に変更

--- lib/constant/team_list.dart
+++ lib/constant/team_list.dart
@@ -1,19 +1,14 @@
-final List<String> npbTeamList = pacificTeamList + centralTeamList;
-
-final pacificTeamList = <String>[
-  'オリックス・バファローズ',
-  '福岡ソフトバンクホークス',
-  '埼玉西武ライオンズ',
-  '東北楽天ゴールデンイーグルス',
-  '千葉ロッテマリーンズ',
-  '北海道日本ハムファイターズ',
-];
-
-final centralTeamList = <String>[
-  '東京ヤクルトスワローズ',
-  '横浜DeNAベイスターズ',
-  '阪神タイガース',
-  '読売ジャイアンツ',
-  '広島東洋カープ',
-  '中日ドラゴンズ',
+final teamList = <Map<String, String>>[
+  {'name': 'オリックス・バファローズ', 'league': 'パ・リーグ'},
+  {'name': '福岡ソフトバンクホークス', 'league': 'パ・リーグ'},
+  {'name': '埼玉西武ライオンズ', 'league': 'パ・リーグ'},
+  {'name': '東北楽天ゴールデンイーグルス', 'league': 'パ・リーグ'},
+  {'name': '千葉ロッテマリーンズ', 'league': 'パ・リーグ'},
+  {'name': '北海道日本ハムファイターズ', 'league': 'パ・リーグ'},
+  {'name': '東京ヤクルトスワローズ', 'league': 'セ・リーグ'},
+  {'name': '横浜DeNAベイスターズ', 'league': 'セ・リーグ'},
+  {'name': '阪神タイガース', 'league': 'セ・リーグ'},
+  {'name': '読売ジャイアンツ', 'league': 'セ・リーグ'},
+  {'name': '広島東洋カープ', 'league': 'セ・リーグ'},
+  {'name': '中日ドラゴンズ', 'league': 'セ・リーグ'},
 ];
