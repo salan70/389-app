fe8a5d26f5a9b1a6c3e9b42d2cd16c9b37a91dfd
Tetsuo.Oda <tetsuo21ad@gmail.com>
2022-12-25 17:05:48 +0900

    🚀 feat : チームのListを作成

---
+++ lib/constant/team_list.dart
@@ -0,0 +1,19 @@
+final List<String> npbTeamList = pacificTeamList + centralTeamList;
+
+final pacificTeamList = <String>[
+  'オリックス・バファローズ',
+  '福岡ソフトバンクホークス',
+  '埼玉西武ライオンズ',
+  '東北楽天ゴールデンイーグルス',
+  '千葉ロッテマリーンズ',
+  '北海道日本ハムファイターズ',
+];
+
+final centralTeamList = <String>[
+  '東京ヤクルトスワローズ',
+  '横浜DeNAベイスターズ',
+  '阪神タイガース',
+  '読売ジャイアンツ',
+  '広島東洋カープ',
+  '中日ドラゴンズ',
+];
