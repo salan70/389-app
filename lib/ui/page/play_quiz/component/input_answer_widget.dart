4bc4e2f151567d09ad07a0233226720bb5dc5bfa
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-08 23:31:08 +0900

    🚀 feat : ダイアログを呼ぶ処理を実装

--- lib/ui/page/play_quiz/component/input_answer_widget.dart
+++ lib/ui/page/play_quiz/component/input_answer_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:textfield_search/textfield_search.dart';
 
 import '../../../../model/ui/hitter_id_by_name.dart';
+import 'incorrect_dialog.dart';
 import 'input_answer_view_model.dart';
 
 class InputAnswerWidget extends ConsumerWidget {
@@ -36,16 +37,37 @@ class InputAnswerWidget extends ConsumerWidget {
           },
         ),
         TextButton(
-          onPressed: () {
-            // TODO(me): 結果のダイアログを表示
-            final isCorrect = viewModel.judgeQuizResult();
-            final resultText = isCorrect ? '正解！' : '残念でしたー😜';
+          onPressed: () async {
+            // TODO(me): 回答が無効な値の場合、ボタンを押せなくする。
+            // あるいは、押したら回答が無効な旨を表示する
 
+            final isCorrect = viewModel.judgeQuizResult();
+
+            // デバッグ用処理
+            // TODO(me): デバッグ不要になり次第削除する
+            final resultText = isCorrect ? '正解！' : '残念でしたー😜';
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text(resultText),
               ),
             );
+
+            // 正解の場合
+            if (isCorrect) {
+              // TODO(me): 結果画面へ遷移
+            }
+            // 不正解の場合
+            else {
+              await showDialog<void>(
+                context: context,
+                barrierDismissible: false,
+                builder: (_) {
+                  return IncorrectDialog(
+                    selectedHitter: textEditingController.text,
+                  );
+                },
+              );
+            }
           },
           child: const Text('回答する'),
         ),
