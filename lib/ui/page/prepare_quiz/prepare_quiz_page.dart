a7a6cdc5d9f9b58de25c035e9edc9fe90ea4537c
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-10 21:21:37 +0900

    ♻️ refactor : StatelessWidgetに修正

--- lib/ui/page/prepare_quiz/prepare_quiz_page.dart
+++ lib/ui/page/prepare_quiz/prepare_quiz_page.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../repository/hitter_search_condition_repository.dart';
-import '../../../state/hitter_search_condition_state.dart';
 import 'component/chose_team_widget.dart';
 import 'component/select_stats_widget.dart';
 import 'component/setting_stats_value_filter_widget.dart';
 import 'component/to_play_quiz_button_widget.dart';
 
-class PrepareQuizPage extends ConsumerWidget {
+class PrepareQuizPage extends StatelessWidget {
   const PrepareQuizPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final hitterSearchCondition = ref.watch(hitterSearchConditionProvider);
-    final hitterSearchConditionRepository =
-        ref.watch(hitterSearchConditionRepositoryProvider);
-
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
