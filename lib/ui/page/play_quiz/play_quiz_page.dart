a76023aaf5546261f051cde7058613aae0c14a69
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-15 16:36:51 +0900

    🎨 ui : デザインを微調整

--- lib/ui/page/play_quiz/play_quiz_page.dart
+++ lib/ui/page/play_quiz/play_quiz_page.dart
@@ -11,9 +11,6 @@ class PlayQuizPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('プレイ'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: ListView(
