703bb2f9c55a53819bad4cd8471ecacc26f43bcb
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-15 16:37:38 +0900

    ♻️ refactor : デバッグ用のコードを削除

--- lib/ui/component/quiz_widget.dart
+++ lib/ui/component/quiz_widget.dart
@@ -29,47 +29,44 @@ class QuizWidget extends ConsumerWidget {
                   ),
               ],
             ),
-            ColoredBox(
-              color: Colors.black26,
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: data.statsMapList.length,
-                itemBuilder: (_, index) {
-                  final statsMap = data.statsMapList[index];
-                  final hiddenStatsIdList = data.hiddenStatsIdList;
+            ListView.builder(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: data.statsMapList.length,
+              itemBuilder: (_, index) {
+                final statsMap = data.statsMapList[index];
+                final hiddenStatsIdList = data.hiddenStatsIdList;
 
-                  return Row(
-                    children: [
-                      for (final selectedStats in selectedStatsList)
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(4),
-                            alignment: Alignment.center,
-                            child: (hiddenStatsIdList.contains(
-                              statsMap[selectedStats]!.id,
-                            ))
-                                ? Container(
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      borderRadius: BorderRadius.circular(4),
-                                    ),
-                                    child: const Text(''),
-                                  )
-                                : FittedBox(
-                                    child: Text(
-                                      statsMap[selectedStats]!.data,
-                                    ),
+                return Row(
+                  children: [
+                    for (final selectedStats in selectedStatsList)
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          alignment: Alignment.center,
+                          child: (hiddenStatsIdList.contains(
+                            statsMap[selectedStats]!.id,
+                          ))
+                              ? Container(
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    borderRadius: BorderRadius.circular(4),
                                   ),
-                          ),
+                                  child: const Text(''),
+                                )
+                              : FittedBox(
+                                  child: Text(
+                                    statsMap[selectedStats]!.data,
+                                  ),
+                                ),
                         ),
-                    ],
-                  );
-                },
-              ),
+                      ),
+                  ],
+                );
+              },
             ),
           ],
         );
