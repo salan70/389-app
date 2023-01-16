1883c36c5f584dbdeddd13e6396f5762c25e0fbb
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-16 22:19:40 +0900

    🚀 feat : TopPageを作成

---
+++ lib/ui/page/top/top_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../prepare_quiz/prepare_quiz_page.dart';
+
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8, top: 80, right: 8, bottom: 8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: const Text('クイズの設定をする'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<Widget>(
+                      builder: (_) => const PrepareQuizPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
