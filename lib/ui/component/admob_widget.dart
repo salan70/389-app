a3aa1d34f122ada25036568f5ebd0b0ef64c82c7
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-29 00:03:16 +0900

    🚀 feat : 環境毎に広告のIDが切り替わる処理を追加

--- lib/ui/component/admob_widget.dart
+++ lib/ui/component/admob_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import '../../util/admob.dart';
 import '../../util/logger.dart';
 
 class BannerAdWidget extends ConsumerWidget {
@@ -12,29 +13,31 @@ class BannerAdWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final bannerAd = BannerAd(
-      adUnitId: Platform.isAndroid
-          ? dotenv.env['TEST_BANNER_ID_ANDROID']!
-          : dotenv.env['TEST_BANNER_ID_IOS']!,
-      size: AdSize.banner,
-      request: const AdRequest(
-        nonPersonalizedAds: true,
-      ),
-      listener: BannerAdListener(
-        onAdFailedToLoad: (Ad ad, LoadAdError error) {
-          ad.dispose();
-          logger.e(error);
-        },
-      ),
-    );
-    bannerAd.load();
-    final adWidget = AdWidget(ad: bannerAd);
+    final futureBannerAd = MyBannerAd().createBannerAd();
 
-    return Container(
-      alignment: Alignment.center,
-      width: bannerAd.size.width.toDouble(),
-      height: bannerAd.size.height.toDouble(),
-      child: adWidget,
+    return FutureBuilder(
+      future: futureBannerAd,
+      builder: (BuildContext context, AsyncSnapshot<BannerAd> snapshot) {
+        if (snapshot.error != null) {
+          logger.e('bannerAd error: ${snapshot.error}');
+          return Container();
+        }
+
+        if (!snapshot.hasData) {
+          return Container();
+        }
+
+        final bannerAd = snapshot.data!;
+        bannerAd.load();
+        final adWidget = AdWidget(ad: bannerAd);
+
+        return Container(
+          alignment: Alignment.center,
+          width: bannerAd.size.width.toDouble(),
+          height: bannerAd.size.height.toDouble(),
+          child: adWidget,
+        );
+      },
     );
   }
 }
--- lib/util/admob.dart
+++ lib/util/admob.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:baseball_quiz_app/util/logger.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 /// InterstitialAdの作成、表示を行うクラス
 class MyInterstitialAd {
@@ -11,9 +12,7 @@ class MyInterstitialAd {
   /// 広告を作成する
   Future<void> createAd() async {
     await InterstitialAd.load(
-      adUnitId: Platform.isAndroid
-          ? dotenv.env['TEST_INTERSTITIAL_ID_ANDROID']!
-          : dotenv.env['TEST_INTERSTITIAL_ID_IOS']!,
+      adUnitId: await _selectInterstitialId(),
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (InterstitialAd ad) {
@@ -22,6 +21,23 @@ class MyInterstitialAd {
         onAdFailedToLoad: logger.e,
       ),
     );
+  }
+
+  Future<String> _selectInterstitialId() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    final appName = packageInfo.appName;
+
+    // prod環境の場合
+    if (appName == '.389') {
+      return Platform.isAndroid
+          ? dotenv.env['INTERSTITIAL_ID_ANDROID']!
+          : dotenv.env['INTERSTITIAL_ID_IOS']!;
+    }
+
+    // prod環境以外の場合
+    return Platform.isAndroid
+        ? dotenv.env['TEST_INTERSTITIAL_ID_ANDROID']!
+        : dotenv.env['TEST_INTERSTITIAL_ID_IOS']!;
   }
 
   /// 広告を表示する
@@ -42,5 +58,40 @@ class MyInterstitialAd {
 
     await _interstitialAd!.show();
     _interstitialAd = null;
+  }
+}
+
+class MyBannerAd {
+  Future<BannerAd> createBannerAd() async {
+    return BannerAd(
+      adUnitId: await _selectBannerId(),
+      size: AdSize.banner,
+      request: const AdRequest(
+        nonPersonalizedAds: true,
+      ),
+      listener: BannerAdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          ad.dispose();
+          logger.e(error);
+        },
+      ),
+    );
+  }
+
+  Future<String> _selectBannerId() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    final appName = packageInfo.appName;
+
+    // prod環境の場合
+    if (appName == '.389') {
+      return Platform.isAndroid
+          ? dotenv.env['BANNER_ID_ANDROID']!
+          : dotenv.env['BANNER_ID_IOS']!;
+    }
+
+    // prod環境以外の場合
+    return Platform.isAndroid
+        ? dotenv.env['TEST_BANNER_ID_ANDROID']!
+        : dotenv.env['TEST_BANNER_ID_IOS']!;
   }
 }
