5494d023c9014cdb55c6175bb74f23e281c465da
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-28 13:22:11 +0900

    🚀 feat : canChangeStateのUnitTestを作成

---
+++ test/ui/page/prepare_quiz/component/select_stats_view_model_test.dart
@@ -0,0 +1,47 @@
+import 'package:baseball_quiz_app/constant/hitter_search_condition_constant.dart';
+import 'package:baseball_quiz_app/ui/page/prepare_quiz/component/select_stats_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('canChangeState', () {
+    test('選択可能上限数 かつ すでに選択されている', () {
+      final result = ProviderContainer()
+          .read(selectStatsViewModelProvider)
+          .canChangeState(selectedLength: mustSelectStatsNum, isSelected: true);
+
+      expect(true, result);
+    });
+
+    test('選択可能上限数 かつ 選択されていない', () {
+      final result =
+          ProviderContainer().read(selectStatsViewModelProvider).canChangeState(
+                selectedLength: mustSelectStatsNum,
+                isSelected: false,
+              );
+
+      expect(false, result);
+    });
+
+    test('選択可能上限数未満', () {
+      final result =
+          ProviderContainer().read(selectStatsViewModelProvider).canChangeState(
+                selectedLength: mustSelectStatsNum - 1,
+                isSelected: false,
+              );
+
+      expect(true, result);
+    });
+
+    // 実際のアプリではこのケースにならない想定
+    test('選択可能上限数より高い', () {
+      final result =
+          ProviderContainer().read(selectStatsViewModelProvider).canChangeState(
+                selectedLength: mustSelectStatsNum + 1,
+                isSelected: false,
+              );
+
+      expect(false, result);
+    });
+  });
+}
