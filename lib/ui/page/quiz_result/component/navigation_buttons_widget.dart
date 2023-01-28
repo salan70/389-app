a67f0d824c6fff58eef15202a57d204a4ce09a7c
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-28 18:21:55 +0900

    ♻️ refactor : 不要なimport文を削除

--- lib/ui/page/quiz_result/component/navigation_buttons_widget.dart
+++ lib/ui/page/quiz_result/component/navigation_buttons_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../state/hitter_quiz_ui_service.dart';
-import '../../play_quiz/play_quiz_page.dart';
 import '../../prepare_quiz/prepare_quiz_page.dart';
 import '../../top/top_page.dart';
 
