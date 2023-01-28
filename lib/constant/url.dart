f472adad65c780ae2e906710139985676c5dc657
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-28 21:38:44 +0900

    🎨 ui : 共有時の文言を修正

--- lib/constant/url.dart
+++ lib/constant/url.dart
@@ -1,2 +1,6 @@
 const termsUrl = 'https://389-terms.web.app/';
+
 const privacyPolicyUrl = 'https://389-privacy-policy.web.app/';
+
+const storeUrl =
+    'https://apps.apple.com/us/app/389-%E3%83%97%E3%83%AD%E9%87%8E%E7%90%83%E6%88%90%E7%B8%BE%E3%82%AF%E3%82%A4%E3%82%BA/id1668555651';
