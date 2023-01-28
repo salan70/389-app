f03d5e705912e75161a45cd9cfe1a23fb8b69971
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-01-29 00:06:40 +0900

    ♻️ refactor : 不要なimport文を削除

--- lib/ui/component/admob_widget.dart
+++ lib/ui/component/admob_widget.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
