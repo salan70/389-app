30604b54cb15631a9a57627fc0de46bef4d7f665
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-04 15:42:44 +0900

    ♻️ refactor : クラス、ファイル名を変更

--- lib/ui/page/quiz_result/component/replay_button.dart
+++ lib/ui/page/quiz_result/component/replay_button.dart
@@ -4,8 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../state/hitter_quiz_ui_service.dart';
 import '../../play_quiz/play_quiz_page.dart';
 
-class ReplayButtonWidget extends ConsumerWidget {
-  const ReplayButtonWidget({
+class ReplayButton extends ConsumerWidget {
+  const ReplayButton({
     super.key,
   });
 
