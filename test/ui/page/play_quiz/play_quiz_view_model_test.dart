653e2a20e5ecc0cfce40f0f86fe2d7e73fb9125b
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-04 15:20:26 +0900

    ♻️ refactor : クイズプレイ画面のViewModelをマージ

--- test/ui/page/play_quiz/play_quiz_view_model_test.dart
+++ test/ui/page/play_quiz/play_quiz_view_model_test.dart
@@ -1,4 +1,4 @@
-import 'package:baseball_quiz_app/ui/page/play_quiz/component/answer_view_model.dart';
+import 'package:baseball_quiz_app/ui/page/play_quiz/play_quiz_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,7 +6,7 @@ void main() {
   group('isShownAds', () {
     test('bool型を返すか確認', () {
       final result =
-          ProviderContainer().read(answerViewModelProvider).isShownAds();
+          ProviderContainer().read(playQuizViewModelProvider).isShownAds();
       expect(bool, result.runtimeType);
     });
   });
