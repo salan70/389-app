960f959bea24845578e4a7569c988148af63b36a
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-04 18:54:37 +0900

    🌙 other : filterHitter関数のテストを作成

--- test/ui/page/play_quiz/play_quiz_view_model_test.dart
+++ test/ui/page/play_quiz/play_quiz_view_model_test.dart
@@ -1,3 +1,5 @@
+import 'package:baseball_quiz_app/model/ui/hitter_id_by_name.dart';
+import 'package:baseball_quiz_app/repository/supabase/supabase_hitter_repository.dart';
 import 'package:baseball_quiz_app/state/hitter_quiz_ui_state.dart';
 import 'package:baseball_quiz_app/ui/page/play_quiz/play_quiz_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,6 +8,110 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../dummy_data/dummy_hitter.dart';
 
 void main() {
+  group('filterHitter関数', () {
+    final hitterList = [
+      const HitterIdByName(label: '坂本勇人（捕手）', id: '1'),
+      const HitterIdByName(label: '坂本勇人（内野手）', id: '2'),
+      const HitterIdByName(label: 'レオネス・マーティン', id: '3'),
+      const HitterIdByName(label: 'T-岡田', id: '4'),
+    ];
+
+    test('5文字（カタカナ）, 1選手に該当', () async {
+      final container = ProviderContainer(
+        overrides: [
+          allHitterListProvider.overrideWith(
+            (ref) => Future.value(hitterList),
+          ),
+        ],
+      );
+      const searchWord = 'マーティン';
+      final result = await container
+          .read(playQuizViewModelProvider)
+          .filterHitter(searchWord);
+
+      final expected = [
+        const HitterIdByName(label: 'レオネス・マーティン', id: '3'),
+      ];
+
+      expect(expected, result);
+    });
+
+    test('2文字（漢字）, 2選手に該当', () async {
+      final container = ProviderContainer(
+        overrides: [
+          allHitterListProvider.overrideWith(
+            (ref) => Future.value(hitterList),
+          ),
+        ],
+      );
+      const searchWord = '坂本';
+      final result = await container
+          .read(playQuizViewModelProvider)
+          .filterHitter(searchWord);
+
+      final expected = [
+        const HitterIdByName(label: '坂本勇人（捕手）', id: '1'),
+        const HitterIdByName(label: '坂本勇人（内野手）', id: '2'),
+      ];
+
+      expect(expected, result);
+    });
+
+    test('1文字（アルファベット）, 1選手に該当', () async {
+      final container = ProviderContainer(
+        overrides: [
+          allHitterListProvider.overrideWith(
+            (ref) => Future.value(hitterList),
+          ),
+        ],
+      );
+      const searchWord = 'T';
+      final result = await container
+          .read(playQuizViewModelProvider)
+          .filterHitter(searchWord);
+
+      final expected = [
+        const HitterIdByName(label: 'T-岡田', id: '4'),
+      ];
+
+      expect(expected, result);
+    });
+
+    test('1文字（漢字）, 該当選手なし', () async {
+      final container = ProviderContainer(
+        overrides: [
+          allHitterListProvider.overrideWith(
+            (ref) => Future.value(hitterList),
+          ),
+        ],
+      );
+      const searchWord = '神';
+      final result = await container
+          .read(playQuizViewModelProvider)
+          .filterHitter(searchWord);
+
+      final expected = <HitterIdByName>[];
+
+      expect(expected, result);
+    });
+
+    test('不正解している', () {
+      final container = ProviderContainer(
+        overrides: [
+          hitterQuizUiStateProvider.overrideWith(
+            (ref) => const AsyncData(dummyHitterQuizUi),
+          ),
+          selectedHitterIdProvider.overrideWith(
+            (ref) => 'incorrect Id',
+          ),
+        ],
+      );
+      final result =
+          container.read(playQuizViewModelProvider).isCorrectHitterQuiz();
+      expect(false, result);
+    });
+  });
+
   group('isCorrectHitterQuiz関数', () {
     test('正解している', () {
       final container = ProviderContainer(
