a2a8fb5ef662260acedb6dec337553f87dae7aa2
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-04 17:23:00 +0900

    🌙 other : canRemoveTeam関数のテストを作成

--- test/ui/page/prepare_quiz/prepare_quiz_view_model_test.dart
+++ test/ui/page/prepare_quiz/prepare_quiz_view_model_test.dart
@@ -1,12 +1,74 @@
+import 'package:baseball_quiz_app/model/typeadapter/hitter_search_condition.dart';
+import 'package:baseball_quiz_app/state/hitter_search_condition_state.dart';
 import 'package:baseball_quiz_app/ui/page/prepare_quiz/prepare_quiz_view_model.dart';
 import 'package:baseball_quiz_app/util/constant/hitter_search_condition_constant.dart';
+import 'package:baseball_quiz_app/util/constant/hitting_stats/stats_type.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final viewModel = ProviderContainer().read(prepareQuizViewModelProvider);
+  group('canRemoveTeam関数', () {
+    test('選択中のteamListの長さが1', () {
+      final hitterSearchCondition = HitterSearchCondition(
+        teamList: [
+          '千葉ロッテマリーンズ',
+        ],
+        minGames: 0,
+        minHits: 0,
+        minHr: 0,
+        selectedStatsList: [
+          StatsType.year.label,
+          StatsType.team.label,
+          StatsType.avg.label,
+          StatsType.hr.label,
+          StatsType.ops.label,
+        ],
+      );
+      final container = ProviderContainer(
+        overrides: [
+          hitterSearchConditionProvider.overrideWith(
+            (ref) => hitterSearchCondition,
+          ),
+        ],
+      );
+      final result =
+          container.read(prepareQuizViewModelProvider).canRemoveTeam();
+      expect(false, result);
+    });
+
+    test('選択中のteamListの長さが2', () {
+      final hitterSearchCondition = HitterSearchCondition(
+        teamList: [
+          '千葉ロッテマリーンズ',
+          '阪神タイガース',
+        ],
+        minGames: 0,
+        minHits: 0,
+        minHr: 0,
+        selectedStatsList: [
+          StatsType.year.label,
+          StatsType.team.label,
+          StatsType.avg.label,
+          StatsType.hr.label,
+          StatsType.ops.label,
+        ],
+      );
+      final container = ProviderContainer(
+        overrides: [
+          hitterSearchConditionProvider.overrideWith(
+            (ref) => hitterSearchCondition,
+          ),
+        ],
+      );
+      final result =
+          container.read(prepareQuizViewModelProvider).canRemoveTeam();
+      expect(true, result);
+    });
+  });
 
   group('isValidChoseTeamList関数', () {
+    final viewModel = ProviderContainer().read(prepareQuizViewModelProvider);
+
     test('最小値-1', () {
       final result = viewModel.isValidChoseTeamList(minChoseTeamNum - 1);
       expect(false, result);
@@ -24,6 +86,8 @@ void main() {
   });
 
   group('canChangeState関数', () {
+    final viewModel = ProviderContainer().read(prepareQuizViewModelProvider);
+
     test('選択可能上限数と同じ かつ すでに選択されている', () {
       final result = viewModel.canChangeState(
         selectedLength: mustSelectStatsNum,
@@ -59,6 +123,8 @@ void main() {
   });
 
   group('isValidSelectedStatsList関数', () {
+    final viewModel = ProviderContainer().read(prepareQuizViewModelProvider);
+
     test('選択可能上限数と同じ', () {
       final result = viewModel.isValidSelectedStatsList(mustSelectStatsNum);
       expect(true, result);
