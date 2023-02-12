a3cef82a1a07f449db14efcc201f4b6d2441b3e8
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-12 22:12:11 +0900

    ♻️ refactor : アーキテクチャ関係のリファクタリング

--- lib/application/admob/interstitial_ad_service.dart
+++ lib/application/admob/interstitial_ad_service.dart
@@ -1,14 +1,23 @@
 import 'dart:io';
+import 'dart:math';
 
-import 'package:baseball_quiz_app/util/logger.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 import '../../util/logger.dart';
+import '../loading/loading_state.dart';
+
+final interstitialAdServiceProvider = Provider(
+  InterstitialAdService.new,
+);
 
 /// InterstitialAd関連の処理を行うサービスクラス
 class InterstitialAdService {
+  InterstitialAdService(this.ref);
+
+  final Ref ref;
   InterstitialAd? _interstitialAd;
 
   /// 広告を作成する
@@ -61,39 +70,20 @@ class InterstitialAdService {
     await _interstitialAd!.show();
     _interstitialAd = null;
   }
-}
 
-class MyBannerAd {
-  Future<BannerAd> createBannerAd() async {
-    return BannerAd(
-      adUnitId: await _selectBannerId(),
-      size: AdSize.banner,
-      request: const AdRequest(
-        nonPersonalizedAds: true,
-      ),
-      listener: BannerAdListener(
-        onAdFailedToLoad: (Ad ad, LoadAdError error) {
-          ad.dispose();
-          logger.e(error);
-        },
-      ),
-    );
+  // TODO(me): 仮置でここのクラスにおいているため、適切なクラスに移動させる
+  /// 結果表示までじらすための処理
+  /// interstitial広告が表示されるために時間を稼ぐという意図もある
+  Future<void> waitResult() async {
+    final loadingNotifier = ref.read(loadingProvider.notifier);
+    loadingNotifier.show();
+    await Future<void>.delayed(const Duration(seconds: 3));
+    loadingNotifier.hide();
   }
 
-  Future<String> _selectBannerId() async {
-    final packageInfo = await PackageInfo.fromPlatform();
-    final appName = packageInfo.appName;
-
-    // prod環境の場合
-    if (appName == '.389') {
-      return Platform.isAndroid
-          ? dotenv.env['BANNER_ID_ANDROID']!
-          : dotenv.env['BANNER_ID_IOS']!;
-    }
-
-    // prod環境以外の場合
-    return Platform.isAndroid
-        ? dotenv.env['TEST_BANNER_ID_ANDROID']!
-        : dotenv.env['TEST_BANNER_ID_IOS']!;
+  /// 広告を表示するかどうかを返す
+  /// 50%の確率で広告を表示する
+  bool isShownAds() {
+    return Random().nextBool();
   }
 }
