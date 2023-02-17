0d5e28dd5d9a6a76b3dae6719105e2ecece70e91
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-17 21:47:59 +0900

    🚀 feat : hitterQuizに不正解数を格納するパラメータを追加

--- lib/presentation/page/play_daily_quiz/component/life_widget.dart
+++ lib/presentation/page/play_daily_quiz/component/life_widget.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../application/quiz/hitter_quiz/hitter_quiz_state.dart';
+
 class LifeWidget extends ConsumerWidget {
   const LifeWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final fullscreenWidth = MediaQuery.of(context).size.width;
-    const incorrectCount = 2;
-
-    Icon favoriteIcon(int borderCount) {
-      if (incorrectCount < borderCount) {
-        return const Icon(Icons.favorite_rounded);
-      }
-      return const Icon(Icons.favorite_border_rounded);
-    }
+    final hitterQuiz = ref.watch(hitterQuizStateProvider);
 
     return Align(
       child: SizedBox(
         width: fullscreenWidth / 2,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            favoriteIcon(1),
-            favoriteIcon(2),
-            favoriteIcon(3),
-          ],
+        child: hitterQuiz.maybeWhen(
+          orElse: Container.new,
+          data: (data) {
+            Icon favoriteIcon(int borderCount) {
+              if (data!.isCorrectCount < borderCount) {
+                return const Icon(Icons.favorite_rounded);
+              }
+              return const Icon(Icons.favorite_border_rounded);
+            }
+
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                favoriteIcon(1),
+                favoriteIcon(2),
+                favoriteIcon(3),
+              ],
+            );
+          },
         ),
       ),
     );
