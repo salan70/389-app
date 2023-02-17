08840e501857f861d4894361270a6219f58392b3
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-17 21:30:28 +0900

    🎨 ui : ライフのUIを実装

--- lib/presentation/page/play_daily_quiz/play_daily_quiz_page.dart
+++ lib/presentation/page/play_daily_quiz/play_daily_quiz_page.dart
@@ -7,6 +7,7 @@ import '../../component/play_quiz/answer_widget.dart';
 import '../../component/play_quiz/quiz_event_buttons.dart';
 import '../../component/play_quiz/retire_button.dart';
 import '../../component/quiz_widget.dart';
+import 'component/life_widget.dart';
 
 class PlayDailyQuizPage extends StatelessWidget {
   const PlayDailyQuizPage({super.key});
@@ -23,19 +24,25 @@ class PlayDailyQuizPage extends StatelessWidget {
             behavior: HitTestBehavior.opaque,
             child: ListView(
               children: const [
+                //TODO: ライフ機能
+                //TODO: Androidのバックキー制御
+                //TODO: Firestoreに保存したデータを取得し、デイリークイズを作成する
+                //TODO: サーバー時間から、デイリークイズに使用するデータを取得する
+                //TODO: プレイ状況/結果をFirestoreに保存
+                //TODO: 共通化したWidgetで、文字列などの切り分けが必要ないか確認
+                //TODO: デイリークイズ用の結果画面を用意
+                //TODO: デイリークイズ用のシェア機能を用意（文言変えるだけっぽそう）
                 BannerAdWidget(),
+                SizedBox(height: 16),
+                LifeWidget(),
                 SizedBox(height: 16),
                 QuizWidget(willUpdate: true),
                 SizedBox(height: 16),
                 QuizEventButtons(),
                 SizedBox(height: 16),
-                AnswerWidget(
-                  retireConfirmText: dailyQuizRetireConfirmText,
-                ),
+                AnswerWidget(retireConfirmText: dailyQuizRetireConfirmText),
                 SizedBox(height: 16),
-                RetireButton(
-                  retireConfirmText: dailyQuizRetireConfirmText,
-                ),
+                RetireButton(retireConfirmText: dailyQuizRetireConfirmText),
                 SizedBox(height: 200),
               ],
             ),
