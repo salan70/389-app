34c1390f13f618782790591ee971fb7cf53f1924
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-18 19:42:44 +0900

    ♻️ refactor : クラス/ディレクトリ/ファイル名を変更

--- lib/presentation/page/top/top_page.dart
+++ lib/presentation/page/top/top_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../component/banner_ad_widget.dart';
 import 'component/to_play_daily_quiz_button.dart';
-import 'component/to_play_quiz_from_top_button.dart';
+import 'component/to_play_normal_quiz_from_top_button.dart';
 import 'component/to_prepare_quiz_button.dart';
 import 'component/to_setting_button.dart';
 
@@ -30,7 +30,7 @@ class TopPage extends StatelessWidget {
                     children: const [
                       ToPrepareQuizButton(),
                       SizedBox(height: 8),
-                      ToPlayQuizFromTopButton(),
+                      ToPlayNormalQuizFromTopButton(),
                       SizedBox(height: 8),
                       ToPlayDailyQuizFromTopButton(),
                       SizedBox(height: 80),
