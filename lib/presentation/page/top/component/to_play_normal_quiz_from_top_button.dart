e423ca0cc6e060f76470b0b2deba4bff35bb6e5c
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-02-18 19:42:44 +0900

    ♻️ refactor : クラス/ディレクトリ/ファイル名を変更

--- lib/presentation/page/top/component/to_play_normal_quiz_from_top_button.dart
+++ lib/presentation/page/top/component/to_play_normal_quiz_from_top_button.dart
@@ -4,20 +4,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../application/quiz/hitter_quiz/hitter_quiz_service.dart';
 import '../../play_normal_quiz/play_normal_quiz_page.dart';
 
-class ToPlayQuizFromTopButton extends ConsumerWidget {
-  const ToPlayQuizFromTopButton({
+class ToPlayNormalQuizFromTopButton extends ConsumerWidget {
+  const ToPlayNormalQuizFromTopButton({
     super.key,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return TextButton(
-      child: FittedBox(
-        child: Text(
-          '保存されている条件でプレイ！',
-          style: Theme.of(context).textTheme.headline5,
-        ),
-      ),
       onPressed: () async {
         // 「Do not use BuildContexts across async gaps.」
         // というLintの警告を回避するためにnavigatorを切り出し
@@ -35,6 +29,12 @@ class ToPlayQuizFromTopButton extends ConsumerWidget {
           ),
         );
       },
+      child: FittedBox(
+        child: Text(
+          '保存されている条件でプレイ！',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
     );
   }
 }
