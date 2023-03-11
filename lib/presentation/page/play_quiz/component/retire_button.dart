ddd4962d8bc4eb8848adcd36923c4a80cac9f7eb
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-11 17:28:50 +0900

    ♻️ refactor : クイズリタイアボタンのテキストや遷移先画面の判別方法を変更

--- lib/presentation/page/play_quiz/component/retire_button.dart
+++ lib/presentation/page/play_quiz/component/retire_button.dart
@@ -1,20 +1,20 @@
+import 'package:baseball_quiz_app/util/constant/hitting_stats_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../application/quiz/hitter_quiz/hitter_quiz_state.dart';
+import '../../../../util/constant/strings_constant.dart';
 import '../../../component/confirm_dialog.dart';
+import '../../quiz_result/daily_quiz_result/daily_quiz_result_page.dart';
+import '../../quiz_result/normal_quiz_result/normal_quiz_result_page.dart';
 
 class RetireButton extends ConsumerWidget {
-  const RetireButton({
-    super.key,
-    required this.retireConfirmText,
-    required this.resultPage,
-  });
-
-  final String retireConfirmText;
-  final Widget resultPage;
+  const RetireButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final quizType = ref.watch(hitterQuizStateProvider).value!.quizType;
+
     return Center(
       child: SizedBox(
         width: 120,
@@ -24,13 +24,18 @@ class RetireButton extends ConsumerWidget {
               context: context,
               barrierDismissible: false,
               builder: (_) {
+                final retireConfirmText = quizType == QuizType.normal
+                    ? normalQuizRetireConfirmText
+                    : dailyQuizRetireConfirmText;
+
+                final resultPage = quizType == QuizType.normal
+                    ? const NormalQuizResultPage()
+                    : const DailyQuizResultPage();
+
                 return ConfirmDialog(
                   confirmText: retireConfirmText,
                   onPressedYes: () {
                     // TODO: ここで結果を保存する
-                    // enumでQuizTypeを作成して、それで判別する。
-                    // hitterQuizにQuizTypeを格納するのもあり
-                    // こうすれば、現在引数で渡しているものも不要になるかも？
                     Navigator.of(context).push(
                       MaterialPageRoute<Widget>(builder: (_) => resultPage),
                     );
