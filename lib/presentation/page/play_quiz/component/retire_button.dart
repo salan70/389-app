5f311851c06cfe9254acbcff5a1dc9b7b7e1be2f
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-11 17:55:57 +0900

    🚀 feat : リタイア時にQuizResultをFirestoreに保存する処理を実装

--- lib/presentation/page/play_quiz/component/retire_button.dart
+++ lib/presentation/page/play_quiz/component/retire_button.dart
@@ -1,3 +1,4 @@
+import 'package:baseball_quiz_app/application/user/user_service.dart';
 import 'package:baseball_quiz_app/util/constant/hitting_stats_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,40 +14,57 @@ class RetireButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final quizType = ref.watch(hitterQuizStateProvider).value!.quizType;
+    final hitterQuiz = ref.watch(hitterQuizStateProvider);
 
-    return Center(
-      child: SizedBox(
-        width: 120,
-        child: TextButton(
-          onPressed: () {
-            showDialog<void>(
-              context: context,
-              barrierDismissible: false,
-              builder: (_) {
-                final retireConfirmText = quizType == QuizType.normal
-                    ? normalQuizRetireConfirmText
-                    : dailyQuizRetireConfirmText;
+    return hitterQuiz.maybeWhen(
+      orElse: Container.new,
+      data: (data) {
+        final quizType = data!.quizType;
+        return Center(
+          child: SizedBox(
+            width: 120,
+            child: TextButton(
+              onPressed: () {
+                showDialog<void>(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (_) {
+                    final retireConfirmText = quizType == QuizType.normal
+                        ? normalQuizRetireConfirmText
+                        : dailyQuizRetireConfirmText;
 
-                final resultPage = quizType == QuizType.normal
-                    ? const NormalQuizResultPage()
-                    : const DailyQuizResultPage();
+                    final resultPage = quizType == QuizType.normal
+                        ? const NormalQuizResultPage()
+                        : const DailyQuizResultPage();
 
-                return ConfirmDialog(
-                  confirmText: retireConfirmText,
-                  onPressedYes: () {
-                    // TODO: ここで結果を保存する
-                    Navigator.of(context).push(
-                      MaterialPageRoute<Widget>(builder: (_) => resultPage),
+                    return ConfirmDialog(
+                      confirmText: retireConfirmText,
+                      onPressedYes: () async {
+                        final userService = ref.read(userServiceProvider);
+
+                        if (quizType == QuizType.normal) {
+                          await userService.createNormalQuizResult();
+                        } else {
+                          await userService.updateDailyQuizResult();
+                        }
+
+                        if (context.mounted) {
+                          await Navigator.of(context).push(
+                            MaterialPageRoute<Widget>(
+                              builder: (_) => resultPage,
+                            ),
+                          );
+                        }
+                      },
                     );
                   },
                 );
               },
-            );
-          },
-          child: const Text('諦める'),
-        ),
-      ),
+              child: const Text('諦める'),
+            ),
+          ),
+        );
+      },
     );
   }
 }
