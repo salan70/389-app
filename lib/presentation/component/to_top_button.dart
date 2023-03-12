e674d238550b32badfcf8298feab2f74d9e974ed
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-12 17:13:50 +0900

    🚀 feat : トップ画面へpopするよう修正

--- lib/presentation/component/to_top_button.dart
+++ lib/presentation/component/to_top_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../page/top/top_page.dart';
-
 class ToTopButton extends ConsumerWidget {
   const ToTopButton({super.key});
 
@@ -10,12 +8,7 @@ class ToTopButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return TextButton(
       onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute<Widget>(
-            builder: (_) => const TopPage(),
-          ),
-        );
+        Navigator.of(context).popUntil((route) => route.isFirst);
       },
       child: const Text('TOPへ戻る'),
     );
