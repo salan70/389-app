d53f5ee04c79995a9b17d93fa0487ead4c1b2a6a
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-15 23:03:06 +0900

    ♻️ refactor : loading関連のファイルの格納先を変更

--- lib/feature/admob/application/interstitial_ad_service.dart
+++ lib/feature/admob/application/interstitial_ad_service.dart
@@ -7,7 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-import '../../../application/loading/loading_state.dart';
+import '../../loading/application/loading_state.dart';
 import '../../../util/logger.dart';
 
 final interstitialAdServiceProvider = Provider(
