a9326cf32cd2a4801eb5915b8b2d100acb9d2572
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-15 20:31:25 +0900

    ♻️ refactor : auth関連のファイルの格納先を変更

--- lib/application/user/user_service.dart
+++ lib/application/user/user_service.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../domain/repository/auth_repository.dart';
 import '../../domain/repository/user_info_repository.dart';
+import '../../feature/auth/domain/auth_repository.dart';
 import '../common/common_state.dart';
 import '../quiz/daily_quiz/daily_quiz_state.dart';
 import '../quiz/hitter_quiz/hitter_quiz_state.dart';
@@ -16,29 +16,6 @@ class UserService {
   const UserService(this.ref);
 
   final Ref ref;
-
-  /// ログインを行う（アプリ起動時に呼ぶ）
-  /// 現在のUserを取得し、nullだったら匿名ユーザーを作成する
-  Future<void> login() async {
-    final authRepository = ref.read(authRepositoryProvider);
-
-    // ログイン済み出ない場合、匿名ユーザーを作成する
-    final user = authRepository.getCurrentUser();
-    if (user == null) {
-      await authRepository.signInAnonymously();
-    }
-
-    // userInfoを更新する
-    await _updateUserInfo();
-  }
-
-  /// userInfoを更新する
-  Future<void> _updateUserInfo() async {
-    final user = ref.read(authRepositoryProvider).getCurrentUser();
-    final userInfoRepository = ref.read(userInfoRepositoryProvider);
-
-    await userInfoRepository.updateUserInfo(user!);
-  }
 
   /// dailyQuizResultを作成する
   Future<void> createDailyQuizResult(String dailyQuizId) async {
