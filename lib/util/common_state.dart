5a2e064f6c35dcae5db5866c77fe83e5c547f035
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-03-28 21:12:54 +0900

    ♻️ refactor : 不要なファイルを削除

--- lib/util/common_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// 処理の状態をAsyncValueとして返すプロバイダー
-final commonFunctionStateProvider = StateProvider<AsyncValue<void>>(
-  (_) => const AsyncValue.data(null),
-);
