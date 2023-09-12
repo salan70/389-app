cc55a912317f3fd04ebd14c4b6d8837d6adce83e
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-09-12 16:44:38 +0900

    🌙 other : シェア時のテキストを変更

--- lib/feature/quiz_result/presentation/gallery_detail/normal_quiz_gallery_detail_page.dart
+++ lib/feature/quiz_result/presentation/gallery_detail/normal_quiz_gallery_detail_page.dart
@@ -17,7 +17,7 @@ class NormalQuizGalleryDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
-    const shareText = '#389quiz\n$appStoreUrl';
+    const shareText = '#プロ野球クイズ #389quiz\n$appStoreUrl';
     const buttonWidth = 160.0;
 
     // TODO(me): globalKeyを引数として渡すのイケてない感ある
