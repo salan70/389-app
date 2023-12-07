c5304291fb91636f9c0d61789b8b118a257b824f
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-12-07 15:05:10 +0900

    feat: :hammer: ローディングとエラーのハンドリングを修正

--- lib/feature/push_notification/presentation/notification_setting_dialog.dart
+++ lib/feature/push_notification/presentation/notification_setting_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:baseball_quiz_app/feature/push_notification/application/local_push_notification_service.dart';
-import 'package:baseball_quiz_app/util/extension/widget_ref_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,7 +6,6 @@ import '../../../../common_widget/my_button.dart';
 import '../../../util/constant/colors_constant.dart';
 import '../application/notification_setting_state.dart';
 
-// TODO(me): エラーハンドリングちゃんとやる
 class NotificationSettingDialog extends ConsumerWidget {
   const NotificationSettingDialog({super.key});
 
@@ -15,71 +13,70 @@ class NotificationSettingDialog extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final asyncNotificationSetting = ref.watch(notificationSettingProvider);
-
-    // loading と error 状態をハンドリング
-    ref.handleAsyncValue<void>(notificationSettingProvider);
-
-    return asyncNotificationSetting.maybeWhen(
-      orElse: Container.new,
-      data: (notificationSetting) {
-        return Dialog(
-          child: SingleChildScrollView(
-            child: SizedBox(
-              child: Column(
-                children: [
-                  const SizedBox(height: 16),
-                  const Text('プッシュ通知の設定'),
-                  const SizedBox(height: 24),
-                  Center(
-                    child: SwitchListTile(
-                      title: const Text('今日の1問 更新のお知らせ'),
-                      value:
-                          notificationSetting.allowStartDailyQuizNotification,
-                      onChanged: (_) => ref
-                          .read(localPushNotificationServiceProvider)
-                          .changeAllowStartDailyQuizNotificationSetting(),
-                      activeColor: highlightColor,
-                    ),
+    // loading , error 時のハンドリングはかなり雑にやっている。
+    //
+    // ローカルDBからのfetchでローディングがかなり短く、エラーも起きにくく、なおかつ、
+    // UI上で ON / OFF を切り替えるたびにローディングを出したくないので、これで許容している。
+    return ref.watch(notificationSettingProvider).maybeWhen(
+          orElse: Container.new,
+          data: (notificationSetting) {
+            return Dialog(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      const Text('プッシュ通知の設定'),
+                      const SizedBox(height: 24),
+                      Center(
+                        child: SwitchListTile(
+                          title: const Text('今日の1問 更新のお知らせ'),
+                          value: notificationSetting
+                              .allowStartDailyQuizNotification,
+                          onChanged: (_) => ref
+                              .read(localPushNotificationServiceProvider)
+                              .changeAllowStartDailyQuizNotificationSetting(),
+                          activeColor: highlightColor,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Center(
+                        child: SwitchListTile(
+                          title: const Text('今日の1問 残り30分のお知らせ'),
+                          value: notificationSetting
+                              .allowRemindDailyQuizNotification,
+                          onChanged: (_) => ref
+                              .read(localPushNotificationServiceProvider)
+                              .changeAllowRemindDailyQuizNotificationSetting(),
+                          activeColor: highlightColor,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Center(
+                        child: SwitchListTile(
+                          title: const Text('その他のお知らせ'),
+                          value: notificationSetting.allowOtherNotification,
+                          onChanged: (_) => ref
+                              .read(localPushNotificationServiceProvider)
+                              .changeAllowOtherNotificationSetting(),
+                          activeColor: highlightColor,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      const Center(
+                        child: SizedBox(
+                          width: _smallButtonWidth,
+                          child: _CloseButton(),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
                   ),
-                  const SizedBox(height: 16),
-                  Center(
-                    child: SwitchListTile(
-                      title: const Text('今日の1問 残り30分のお知らせ'),
-                      value:
-                          notificationSetting.allowRemindDailyQuizNotification,
-                      onChanged: (_) => ref
-                          .read(localPushNotificationServiceProvider)
-                          .changeAllowRemindDailyQuizNotificationSetting(),
-                      activeColor: highlightColor,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Center(
-                    child: SwitchListTile(
-                      title: const Text('その他のお知らせ'),
-                      value: notificationSetting.allowOtherNotification,
-                      onChanged: (_) => ref
-                          .read(localPushNotificationServiceProvider)
-                          .changeAllowOtherNotificationSetting(),
-                      activeColor: highlightColor,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Center(
-                    child: SizedBox(
-                      width: _smallButtonWidth,
-                      child: _CloseButton(),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         );
-      },
-    );
   }
 }
 
