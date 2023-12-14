65e6e6d02121292665484cdee2edd96effde54cf
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-12-14 17:42:35 +0900

    feat: :lipstick: empty state を作成

--- lib/feature/quiz_result/presentation/gallery_list/normal_quiz_gallery_list_page.dart
+++ lib/feature/quiz_result/presentation/gallery_list/normal_quiz_gallery_list_page.dart
@@ -21,58 +21,70 @@ class NormalQuizGalleryListPage extends ConsumerWidget {
           padding: const EdgeInsets.all(16),
           child: ListView(
             children: [
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: quizResultList.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                  childAspectRatio: 3 / 2,
-                ),
-                itemBuilder: (context, index) {
-                  final quizResult = quizResultList[index];
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<Widget>(
-                          builder: (_) => NormalQuizGalleryDetailPage(
-                            quizResult: quizResult,
-                          ),
-                          settings: const RouteSettings(
-                            name: '/normal_quiz_gallery_detail_page',
-                          ),
-                        ),
-                      );
-                    },
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: backgroundColor,
-                        border: Border.all(color: primaryColor, width: 2),
-                        borderRadius: BorderRadius.circular(8),
+              quizResultList.isEmpty
+                  ? const Center(
+                      child: Text(
+                        'まだ履歴がありません。\n'
+                        'プレイしたクイズの履歴が表示されます。',
+                        textAlign: TextAlign.center,
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          quizResult.resultRank.smallLabellWidget,
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(quizResult.updatedAt.toFormattedString()),
-                              FittedBox(
-                                child: Text('${quizResult.unveilPercentage}%表示'
-                                    '（${quizResult.unveilCount}/${quizResult.totalStatsCount})'),
+                    )
+                  : GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: quizResultList.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 16,
+                        crossAxisSpacing: 16,
+                        childAspectRatio: 3 / 2,
+                      ),
+                      itemBuilder: (context, index) {
+                        final quizResult = quizResultList[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute<Widget>(
+                                builder: (_) => NormalQuizGalleryDetailPage(
+                                  quizResult: quizResult,
+                                ),
+                                settings: const RouteSettings(
+                                  name: '/normal_quiz_gallery_detail_page',
+                                ),
                               ),
-                            ],
+                            );
+                          },
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: backgroundColor,
+                              border: Border.all(color: primaryColor, width: 2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                quizResult.resultRank.smallLabellWidget,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      quizResult.updatedAt.toFormattedString(),
+                                    ),
+                                    FittedBox(
+                                      child: Text(
+                                          '${quizResult.unveilPercentage}%表示'
+                                          '（${quizResult.unveilCount}/${quizResult.totalStatsCount})'),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
               const SizedBox(height: 120),
             ],
           ),
