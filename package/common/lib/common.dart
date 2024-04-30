bbc862491dda5796e8462a272a4aa8f1963c810f
Tetsuo.Oda <tetsuo21ad@gmail.com>
2024-04-30 15:48:39 +0900

    refactor: :building_construction: logger の格納場所を変更

--- package/common/lib/common.dart
+++ package/common/lib/common.dart
@@ -2,5 +2,6 @@ library common;
 
 export 'src/firebase/firebase.dart';
 export 'src/flavor/flavor.dart';
+export 'src/logger/logger.dart';
 export 'src/supabase/supabase.dart';
 export 'src/util/util.dart';
--- package/common/lib/src/util/util.dart
+++ package/common/lib/src/util/util.dart
@@ -3,4 +3,4 @@ export 'exception/app_exception.dart';
 export 'exception/auth_exception.dart';
 export 'exception/firestore_exception.dart';
 export 'exception/supabase_exception.dart';
-export 'logger.dart';
+export '../logger/logger.dart';
