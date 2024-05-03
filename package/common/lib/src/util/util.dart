847ad45ad6b8c060e217c01fa9b5c2c0a26c76e7
Tetsuo.Oda <tetsuo21ad@gmail.com>
2024-05-03 13:35:21 +0900

    refactor: :fire: 不要なコードを削除

--- package/common/lib/src/util/util.dart
+++ package/common/lib/src/util/util.dart
@@ -3,4 +3,3 @@ export 'exception/app_exception.dart';
 export 'exception/auth_exception.dart';
 export 'exception/firestore_exception.dart';
 export 'exception/supabase_exception.dart';
-export '../logger/logger.dart';
