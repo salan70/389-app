332051f4b153246749d509adf90e3af28a9dcca9
Tetsuo.Oda <tetsuo21ad@gmail.com>
2024-05-19 15:16:55 +0900

    feat: :pushpin: 対象となるシーズンを取得する処理を実装

--- package/model/lib/src/feature/season/infrastructure/season_repository.dart
+++ package/model/lib/src/feature/season/infrastructure/season_repository.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:common/common.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../util/season_type.dart';
@@ -5,13 +7,31 @@ import '../util/season_type.dart';
 part 'season_repository.g.dart';
 
 @riverpod
-SeasonRepository seasonRepository(SeasonRepositoryRef ref) =>
-    SeasonRepository();
+SeasonRepository seasonRepository(SeasonRepositoryRef ref) {
+  final firestore = ref.watch(firestoreProvider);
+  return SeasonRepository(firestore);
+}
 
 class SeasonRepository {
+  SeasonRepository(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
   /// [baseDate]を基準として、対象となるシーズンを取得する。
   Future<SeasonType> fetchTargetSeason(DateTime baseDate) async {
-    // todo: 実装する。
-    return SeasonType.end2022;
+    final QuerySnapshot querySnapshot = await _firestore
+        .collection('seasonPeriods')
+        .where('startDate', isLessThanOrEqualTo: baseDate)
+        .orderBy('startDate', descending: true)
+        .limit(1)
+        .get();
+
+    if (querySnapshot.docs.isEmpty) {
+      // * 対象ドキュメントが見つからなかった場合
+      throw FistoreException.notFound();
+    }
+    // * 対象ドキュメントが見つかった場合
+    final DocumentSnapshot document = querySnapshot.docs.first;
+    return SeasonType.fromFirestoreValue(document['seasonType'] as String);
   }
 }
