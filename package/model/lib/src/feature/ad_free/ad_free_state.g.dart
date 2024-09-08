f3e45bb50289418bdc99744c0a6d59ca6c83f853
salan70 <tetsuo21ad@gmail.com>
2024-09-08 17:59:13 +0900

    feat: 広告非表示期間中はインタースティシャル広告を非表示にするよう改修 :sparkles:

---
+++ package/model/lib/src/feature/ad_free/ad_free_state.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ad_free_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$endAtAdFreePeriodHash() => r'553cc963706848e364cb6f2fb8fb747e1ae1d5e5';
+
+/// 広告非表示期間の終了日時を取得する。
+///
+/// 広告非表示期間が設定されていない場合は `null` を返す。
+///
+/// Copied from [endAtAdFreePeriod].
+@ProviderFor(endAtAdFreePeriod)
+final endAtAdFreePeriodProvider = AutoDisposeFutureProvider<DateTime?>.internal(
+  endAtAdFreePeriod,
+  name: r'endAtAdFreePeriodProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$endAtAdFreePeriodHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef EndAtAdFreePeriodRef = AutoDisposeFutureProviderRef<DateTime?>;
+String _$isAdFreePeriodHash() => r'7a8e2e7efb588ad3ec622d08f0f36c6aadd3b8bb';
+
+/// 広告非表示期間中かどうかを返す。
+///
+/// Copied from [isAdFreePeriod].
+@ProviderFor(isAdFreePeriod)
+final isAdFreePeriodProvider = AutoDisposeFutureProvider<bool>.internal(
+  isAdFreePeriod,
+  name: r'isAdFreePeriodProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$isAdFreePeriodHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef IsAdFreePeriodRef = AutoDisposeFutureProviderRef<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
