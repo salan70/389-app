83809f9a7846d14c6d88b69f1a07e2ec66916e89
salan70 <tetsuo21ad@gmail.com>
2024-09-08 18:03:10 +0900

    feat: 広告非表示期間中はバナー広告を非表示にするよう改修 :sparkles:

--- package/app/lib/ui/component/ad/banner_ad_widget.dart
+++ package/app/lib/ui/component/ad/banner_ad_widget.dart
@@ -2,6 +2,7 @@ import 'package:common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:model/model.dart';
 
 class BannerAdWidget extends ConsumerWidget {
   const BannerAdWidget({super.key});
@@ -11,22 +12,34 @@ class BannerAdWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox(
-      height: _buttonHeight,
-      width: _buttonWidth,
-      child: AdWidget(
-        ad: BannerAd(
-          size: AdSize.banner,
-          adUnitId: ref.watch(flavorProvider).bannerId,
-          request: const AdRequest(),
-          listener: BannerAdListener(
-            onAdFailedToLoad: (Ad ad, LoadAdError error) {
-              ad.dispose();
-              logger.e('BannerAdの読み込みに失敗しました。 error: $error');
-            },
+    final asyncIsAdFree = ref.watch(isAdFreePeriodProvider);
+
+    return asyncIsAdFree.maybeWhen(
+      orElse: () => const SizedBox.shrink(),
+      data: (isAdFree) {
+        // 広告非表示期間中は広告を表示しない。
+        if (isAdFree) {
+          return const SizedBox.shrink();
+        }
+
+        return SizedBox(
+          height: _buttonHeight,
+          width: _buttonWidth,
+          child: AdWidget(
+            ad: BannerAd(
+              size: AdSize.banner,
+              adUnitId: ref.watch(flavorProvider).bannerId,
+              request: const AdRequest(),
+              listener: BannerAdListener(
+                onAdFailedToLoad: (Ad ad, LoadAdError error) {
+                  ad.dispose();
+                  logger.e('BannerAdの読み込みに失敗しました。 error: $error');
+                },
+              ),
+            )..load(),
           ),
-        )..load(),
-      ),
+        );
+      },
     );
   }
 }
